75f464f9485e90077215d0053b081b11214737c9
Victoria Neustroeva <neustrvk@gmail.com>
2026-06-11 11:52:36 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const inputElements = [...document.querySelectorAll('input')];
+
+inputElements.forEach((input) => {
+  input.setAttribute('placeholder', formatCamelCase(input.name));
+
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.name;
+  label.textContent = formatCamelCase(input.name);
+
+  input.before(label);
+});
+
+function formatCamelCase(str) {
+  const withSpaces = str.replaceAll(/([A-Z])/g, ' $1');
+
+  return withSpaces[0].toUpperCase() + withSpaces.slice(1);
+}
